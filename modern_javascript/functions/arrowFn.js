5e8a0d956f052023f32c3b2bc4e68372256cf737
musharruf <mdmusharruf13@gmail.com>
2024-11-27 11:18:06 +0530

    arrow fn example added

--- modern_javascript/functions/arrowFn.js
+++ modern_javascript/functions/arrowFn.js
@@ -16,3 +16,19 @@ const sumOfTwo = (num1, num2) => {
 }
 
 logMsg(sumOfTwo(2, 3));
+
+
+
+/**
+ * In regular functions the 'this' keyword represents the object that called the function, which could be the window, the document, a button or whatever.
+ * with arrow function the 'this' keyword always represents the object that defined the arrow function
+ */
+
+const fn1 = () => {
+    name = "musharruf";
+    age = 45;
+    console.log(this.name);
+}
+
+fn1();
+
