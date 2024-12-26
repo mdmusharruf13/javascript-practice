ee53f7d4bb7f37c28201f71e428dcb4a4938e13c
musharruf <mdmusharruf13@gmail.com>
2024-12-27 00:22:33 +0530

    notes on super()

--- modern_javascript/OOPS/superMthd.js
+++ modern_javascript/OOPS/superMthd.js
@@ -20,4 +20,13 @@ class SuperBike extends Bike {
 
 const myBike = new SuperBike(110, 6);
 console.log(myBike.getCurrentSpeed());
-console.log(myBike.getMaxSpeed()); 
+console.log(myBike.getMaxSpeed());
+
+
+/** 
+ * Note: 
+ * In JavaScript, when a subclass is created using the extends keyword, the parent class's constructor must be invoked explicitly using super() if the sub class has a constructor. Failing to do so will result in an error, as the this keyword cannot be used before calling super().
+ * If the subclass has an empty constructor, calling super() is still required to properly initialize the object.
+ * If the parent constructor expects parameters, you must pass them through super().
+ * The this keyword cannot be used in a subclass constructor untill super() is called.
+ */
