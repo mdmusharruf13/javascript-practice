6ea8a2f889899fc38e24ba0b2988fca0e6e92571
musharruf <mdmusharruf13@gmail.com>
2025-01-01 23:00:53 +0530

    fetching data with promise

--- modern_javascript/Promises/promise.js
+++ modern_javascript/Promises/promise.js
@@ -25,3 +25,21 @@ promise
     .catch(error => {
         console.log(error);
     })
+
+
+const fetchData = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+    });
+};
+
+fetchData('https://jsonplaceholder.typicode.com/todos/1')
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log("error: ", error);
+    });
