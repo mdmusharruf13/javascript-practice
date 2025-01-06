37c743029c609d215bacf36ec10d3cca06d29c10
musharruf <mdmusharruf13@gmail.com>
2025-01-06 10:26:25 +0530

    parsing object's porperty (key)

--- javascript_challenges/object1.js
+++ javascript_challenges/object1.js
@@ -1,5 +1,4 @@
 const a = {};
-
 const b = {
     val: 1
 };
@@ -8,12 +7,16 @@ const c = {
     val: 2
 };
 
-a[b] = {
+a[JSON.stringify(b)] = {
     val: 5
 };
 
+a[b] = {
+    val: 5
+};
 a[c] = {
     val: 9
 }
 
 console.log(a[b]);
+console.log(a[JSON.stringify(b)]);
