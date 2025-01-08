28a7831d2ec4c28438b1210816b1501dd7c117c4
musharruf <mdmusharruf13@gmail.com>
2025-01-08 07:37:54 +0530

    array and string methods

--- modern_javascript/Arrays/practice1.js
+++ modern_javascript/Arrays/practice1.js
@@ -35,3 +35,45 @@ console.log(multiDim.flat());
 console.log(multiDim.flat(4));
 
 console.log(arr1.flatMap(n => [n, n * 2]));
+
+console.log(arr.includes(5));
+
+let strs = "javascript is awesome";
+console.log(strs.includes("ip"));
+console.log(strs.indexOf('e'));
+
+console.log(arr.indexOf(2));
+console.log(arr.lastIndexOf(2));
+
+console.log(arr.join("|"));
+console.log(arr)
+
+for (let [key, value] of [arr3.keys(), arr3.values()]) {
+    console.log(`arr3[${key}] = ${value}`);
+}
+
+console.log(arr3);
+console.log(arr3.pop());
+console.log(arr3.push("25"));
+console.log(arr3);
+
+console.log(arr3.unshift(33));
+console.log(arr3);
+console.log(arr3.shift());
+console.log(arr3);
+
+let newArr = [1, 2, 3, 4, 5];
+console.log(newArr.reduce((sum, num) => sum + num, 10));
+
+console.log(newArr.reduceRight((total, current, index) => {
+    console.log(total, current, index);
+    return total + current;
+}));
+
+console.log(['a', 'b', 'c', 'd'].reduce((str, curr) => str + curr));
+
+console.log(newArr.reverse());
+console.log(newArr);
+
+console.log(["1", "2", "11"].sort());
+console.log(["1", "2", "11"].sort((a, b) => a - b));
