ccb1a682a4f97ee876345c4b9860d51fc73539cf
musharruf <mdmusharruf13@gmail.com>
2025-01-08 07:43:31 +0530

    string methods practice

---
+++ modern_javascript/Strings/practice1.js
@@ -0,0 +1,25 @@
+let str = "ha";
+
+console.log(str);
+console.log(str.repeat(3));
+console.log(str);
+
+let name = "javascript is awesome";
+console.log(name.replaceAll("a", "A"));
+console.log(name);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr.slice(5));
+console.log(arr);
+
+console.log(arr.splice(8, 3));
+console.log(arr);
+
+console.log(name.slice(10));
+console.log(name.slice(10, 16));
+
+console.log(name.startsWith("java"));
+console.log(name.startsWith("Java"));
+
+console.log(name.substring());
+
