f3f578eb2b634de41d36e16960f69012714aa8e3
musharruf <mdmusharruf13@gmail.com>
2025-01-15 16:27:18 +0530

    arrow function as  arrow function with `this` keyword

--- javascript_challenges/this1.js
+++ javascript_challenges/this1.js
@@ -2,8 +2,19 @@ const obj = {
     a: 10,
     x: function () {
         console.log(this);
+    },
+    y: () => {
+        console.log(this);
+    },
+    z: {
+        age: 100,
+        getContext: () => {
+            console.log("context", this);
+        }
     }
 }
 
 this.name = "this";
 obj.x();
+obj.y();
+obj.z.getContext();
