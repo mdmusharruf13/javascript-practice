3654e651a6e6cfce48b6a3526fce10fc53749b61
musharruf <mdmusharruf13@gmail.com>
2025-01-15 17:26:08 +0530

    `this` inside nested regular function

--- javascript_challenges/this1.js
+++ javascript_challenges/this1.js
@@ -18,3 +18,19 @@ this.name = "this";
 obj.x();
 obj.y();
 obj.z.getContext();
+
+
+const obj1 = {
+    num: 45,
+    arrowMethod: () => {
+        console.log('Arrow function\'s this', this);
+
+        function regularFn() {
+            console.log('Regular function\'s this:', this);
+        }
+
+        regularFn();
+    }
+};
+
+obj1.arrowMethod();
