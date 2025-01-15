42ccc869b91f7dc41126e52666b72ac512cb1578
musharruf <mdmusharruf13@gmail.com>
2025-01-15 17:47:14 +0530

    example 2: `this` inside nested-nested arrow function

--- javascript_challenges/thisAndArrowFn.js
+++ javascript_challenges/thisAndArrowFn.js
@@ -22,3 +22,32 @@ const obj1 = {
 }
 
 obj1.arrowMethod();
+
+
+// example 2
+const obj2 = {
+    value: "outer value",
+    arrowFn: () => {
+        console.log("outer arrow function's this: ", this);
+
+        function regularOuterFn() {
+            console.log("regular outer function's this: ", this);
+
+            function regularInnerFn() {
+                console.log("regular inner function's this: ", this);
+
+                const nestedArrowFn = () => {
+                    console.log("nested arrow function's this: ", this);
+                }
+
+                nestedArrowFn();
+            }
+
+            regularInnerFn();
+        }
+
+        regularOuterFn();
+    }
+}
+
+obj2.arrowFn();
