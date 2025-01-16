6423f9cf565bbe6668a316ca71992484ad7c267d
musharruf <mdmusharruf13@gmail.com>
2025-01-16 23:56:07 +0530

    similar to constructor function

--- javascript_challenges/objectP2.js
+++ javascript_challenges/objectP2.js
@@ -11,14 +11,30 @@ function Student(name, rollNo) {
     this.rollNo = rollNo;
 }
 
-const std = new Student("mush", 598);
+const std1 = new Student("mush", 598);
 
 Student.prototype.getName = function () {
     return `My name is ${this.name}`
 }
 
-console.log(std.getName());
+console.log(std1.getName());
 
 const std2 = new Student("nawaz", 98);
 
 console.log(std2.getName());
+
+// similar mimic
+function createObj(name, age) {
+    const obj = { name, age };
+    return obj;
+}
+
+const obj1 = createObj("superman", 45);
+console.log(obj1);
+console.log(std1);
+
+function getDetails() {
+    return `${this.name} is ${this.age} years old.`;
+}
+
+console.log(getDetails.call(obj1));
