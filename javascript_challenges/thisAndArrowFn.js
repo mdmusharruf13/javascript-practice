f1b409e9a597490f7fe96cd7f91a4b5cfe83f584
musharruf <mdmusharruf13@gmail.com>
2025-01-17 21:58:34 +0530

    `this` value inside multiple nested function

--- javascript_challenges/thisAndArrowFn.js
+++ javascript_challenges/thisAndArrowFn.js
@@ -1,4 +1,5 @@
 // example 1
+console.log("example 1:")
 const obj1 = {
     value: "outer",
     arrowMethod: () => {
@@ -25,6 +26,7 @@ obj1.arrowMethod();
 
 
 // example 2
+console.log("example 2:")
 const obj2 = {
     value: "outer value",
     arrowFn: () => {
@@ -35,12 +37,12 @@ const obj2 = {
 
             function regularInnerFn() {
                 console.log("regular inner function's this: ", this);
-
                 const nestedArrowFn = () => {
                     console.log("nested arrow function's this: ", this);
                 }
 
                 nestedArrowFn();
+
             }
 
             regularInnerFn();
@@ -51,3 +53,33 @@ const obj2 = {
 }
 
 obj2.arrowFn();
+
+
+// example 3
+console.log("example 3:");
+const obj3 = {
+    value: "third object",
+    arrowFn: () => {
+        console.log("arrow function this: ", this);
+
+        const innerArrowFn = () => {
+            console.log("inner arrow function this: ", this);
+
+            function regularFn() {
+                console.log("regular function this: ", this);
+
+                const nestedArrowFn = () => {
+                    console.log("nested arrow function this: ", this);
+                }
+
+                nestedArrowFn();
+            }
+
+            regularFn();
+        }
+
+        innerArrowFn();
+    }
+}
+
+obj3.arrowFn();
