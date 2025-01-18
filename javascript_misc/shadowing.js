18689c5161e87e386ac1e20f6ae6783c322277c4
musharruf <mdmusharruf13@gmail.com>
2025-01-18 13:05:58 +0530

    illegal shadowing

--- javascript_misc/shadowing.js
+++ javascript_misc/shadowing.js
@@ -9,7 +9,7 @@ function test() {
     console.log(a)
 }
 
-test();
+// test();
 
 /**
  * Note: 
@@ -18,3 +18,19 @@ test();
  * if we shadow let variable by var variable it is known as illegal shadowing.
  * if we do so it throws error that variable is already defined.
  */
+
+// illegal shadowing
+function check() {
+    var a = "hello";
+    let b = "hi";
+
+    if (true) {
+        let a = "Hello...";
+        var b = "Hi...";  // throws error
+        console.log(a);
+        console.log(b);
+    }
+    console.log(this);
+}
+
+check();
