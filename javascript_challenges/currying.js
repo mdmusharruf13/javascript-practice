735bb33007c69d009696d7b86f5f5b9862f3412d
musharruf <mdmusharruf13@gmail.com>
2025-01-21 21:32:17 +0530

    normal function to curry function

--- javascript_challenges/currying.js
+++ javascript_challenges/currying.js
@@ -1,3 +1,4 @@
+// infinite currying
 function calculate(n1) {
     return (n2) => {
         if (!n2) return n1;
@@ -8,3 +9,24 @@ function calculate(n1) {
 const multiply = calculate(5)(2)(2)(2)();
 
 console.log(multiply);
+
+
+// convert f(a,b,c,d) => f(a)(b)(c)(d)
+function curry(func) {
+    return function curriedFun(...args) {
+        if (args.length >= func.length) {
+            return func(...args);
+        }
+        else {
+            return function (...next) {
+                return curriedFun(...args, ...next);
+            }
+        }
+    }
+}
+
+const sum = (a, b, c, d) => a + b + c + d;
+
+const curryFn = curry(sum);
+
+console.log(curryFn(1)(2)(2)(10));
