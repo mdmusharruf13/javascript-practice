28c59005190975d9ee6021da08b9b903cd897dad
musharruf <mdmusharruf13@gmail.com>
2025-01-25 21:55:01 +0530

    changing context using apply()

--- modern_javascript/functions/apply.js
+++ modern_javascript/functions/apply.js
@@ -7,3 +7,21 @@ function sayHello(age) {
 sayHello(35);
 
 sayHello.apply(obj, [30]);
+
+
+var obj = { name: "mush" };
+
+function getMarks(marks) {
+    let totalMarks = marks.reduce((total, current) => {
+        return total + current;
+    });
+    return `${this.name} scored ${totalMarks} marks`;
+}
+
+let result = getMarks([15, 18, 20, 16, 15], 2);
+console.log(result);
+
+
+const std1 = { name: "musharruf" };
+let result2 = getMarks.apply(std1, [[16, 19, 13, 20, 20]]);
+console.log(result2);
