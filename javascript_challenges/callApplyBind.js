6eab79f28bbaf4790ff439971fecd8a4f69f47a5
musharruf <mdmusharruf13@gmail.com>
2025-01-26 17:46:59 +0530

    min/max with apply()

--- javascript_challenges/callApplyBind.js
+++ javascript_challenges/callApplyBind.js
@@ -21,3 +21,14 @@ const elements = [0, 1, 2, 3];
 arr.push.apply(arr, elements);
 
 console.log(arr);
+
+
+// find min/max number in an array using apply()
+const nums = [5, 6, 2, 3, 7];
+console.log(Math.max(5, 9));
+console.log(Math.max(2, 3, 4, 1));
+console.log(Math.max(...nums));
+console.log(Math.max(nums)); // NaN
+
+console.log(Math.max.apply(null, nums));
+console.log(Math.min.apply(null, nums));
