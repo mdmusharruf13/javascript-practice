b3868d37742883d502dcac59f8c589b604c40ab9
musharruf <mdmusharruf13@gmail.com>
2025-02-04 22:04:31 +0530

    `this` inside method and regular function

--- javascript_challenges/this1.js
+++ javascript_challenges/this1.js
@@ -34,3 +34,20 @@ const obj1 = {
 };
 
 obj1.arrowMethod();
+
+
+// `this` value inside method and regular function
+var myObject = {
+    name: "mush",
+    func: function () {
+        var self = this;
+        console.log("outer func: this.name:", this.name);
+        console.log("outer func: self.name:", self.name);
+        (function () {
+            console.log("inner func: this.name:", this.name);
+            console.log("inner func: self.name:", self.name);
+        })()
+    }
+}
+
+myObject.func();
