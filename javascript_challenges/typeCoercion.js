1593a5b3f741a12fa35726d1c79a85dd4b8cbb12
musharruf <mdmusharruf13@gmail.com>
2025-02-05 23:18:26 +0530

    type coercion

---
+++ javascript_challenges/typeCoercion.js
@@ -0,0 +1,22 @@
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2)
+
+console.log(typeof null);
+
+console.log('5' - 2);
+console.log('5' + 2);
+
+console.log(false == '0');
+console.log(false === '0');
+
+console.log(typeof NaN);
+console.log(NaN === NaN);
+
+console.log(typeof ([] + []));
+console.log([] + {});
+console.log({} + []);
+
+console.log(!!"");
+console.log(!!" ");
+console.log(!!null);
+console.log(!!undefined);
