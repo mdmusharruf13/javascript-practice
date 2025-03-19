b8c56f90c71e94817b4af9418dcb7daee1f9c5cb
musharruf <mdmusharruf13@gmail.com>
2025-03-19 11:30:38 +0530

    iterating array: forEach() & for loop

--- javascript_challenges/arrays1.js
+++ javascript_challenges/arrays1.js
@@ -1,29 +1,35 @@
 const arr = [1, 2, 3];
-
 arr[4] = 10;
-
 console.log(arr);
-
-const [a, b] = [10, "hii"];
-
-console.log(a, b);
 
 const [, , , num] = [10, 20, 30, 40, 50];
 console.log(num);
 
 const [n1, n2] = [10, 20, 30, 50, 100];
-console.log(n1);
-console.log(n2);
+console.log(`n1: ${n1}, n2: ${n2}`);
 
+// accessing nth indexed value
 const { 3: n } = [11, 12, 13, 45, 55];
 console.log(n);
 
 let arr1 = [2, 6, 8, 9, 1, 2, 5];
-console.log(arr1.sort((a, b) => a - b));
+console.log(arr1.sort((a, b) => a - b)); // ascending
+console.log(arr1.sort((a, b) => b - a)); // descending
 
+
+let arr2 = [1, 2, 3];
+arr2[10] = 99;
+console.log(arr2.length);
+
+// ignores undefined values
+arr2.forEach((v, i) => console.log(v, i));
+
+for (let val of arr2) {
+    console.log(val);
+}
 
 // duplicating array elements
-// find issue in the code below
+// // find issue in the code below
 function duplicate(arr) {
     for (var i = 0; i < arr.length; i++) {
         arr.push(arr[i]);
@@ -34,7 +40,3 @@ function duplicate(arr) {
 let array = [1, 2, 3];
 const newArr = duplicate(array);
 console.log(newArr);
-
-let arr2 = [1, 2, 3];
-arr2[10] = 99;
-console.log(arr2.length);
